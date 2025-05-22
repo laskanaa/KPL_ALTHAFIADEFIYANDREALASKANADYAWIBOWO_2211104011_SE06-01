b5c60633737338718d97e665236420404b96196c
Althafia Defiyandrea Laskanadya Wibowo <162089627+laskanaa@users.noreply.github.com>
2025-05-22 16:38:16 +0700

    07
    07

---
+++ 07_Grammar-Based_Input_Processing_Parsing/07.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Identitas
+        string nama = "Althafia Defiyandrea Laskanadya Wibowo";
+        string nim = "2211104011";
+        string fakultas = "Fakultas Informatika";
+
+        Console.WriteLine($"Nama {nama} dengan NIM {nim} dari {fakultas}\n");
+
+        // Daftar mata kuliah
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        Console.WriteLine("MK 1 CCK2KAB4 - Konstruksi Perangkat Lunak");
+        Console.WriteLine("MK 2 CCK3KAB3 - Manajemen Konfigurasi Dan Evolusi Perangkat Lunak");
+        Console.WriteLine("MK 3 CCK2LAB3 - Kecerdasan Buatan");
+        Console.WriteLine("MK 4 CCK2DAB3 - Proses Perangkat Lunak");
+        Console.WriteLine("MK 5 CCK1LAB3 - Design Thinking");
+        Console.WriteLine("MK 6 CCK2JAC2 - Proyek Tingkat 2");
+        Console.WriteLine("MK 7 CCK3IAB2 - Tata Tulis Ilmiah");
+    }
+}
